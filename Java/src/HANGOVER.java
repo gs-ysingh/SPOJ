d1119622a792a5d46775827c5d9dbac62cd728fa
Yogesh Singh <ysingh@gainsight.com>
2017-02-14 23:31:50 +0530

    Writing this code on valantine day, feeling lazy

---
+++ Java/src/HANGOVER.java
@@ -0,0 +1,32 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * Created by YSingh on 14/02/17.
+ */
+public class HANGOVER {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double value = Double.parseDouble(sc.nextLine());
+        int n = 1;
+        Double sum = (double) Math.round(100.0 / (n + 1)) / 100.0;
+        int count = 0;
+        while (value != 0.0) {
+            count = 1;
+            while (sum < value ) {
+                count++;
+                n++;
+                sum = (double) Math.round((sum + (1 / (n + 1)))* 100.0 / 100.0);
+            }
+            System.out.println(count);
+            value = Float.parseFloat(sc.nextLine());
+        }
+
+    }
+
+    public static BigDecimal round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd;
+    }
+}
