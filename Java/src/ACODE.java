d906d41f1816f6dd5825b092bfc754e5a3883056
Yogesh Singh <ysingh@gainsight.com>
2017-02-19 23:06:47 +0530

    Removed previous code

--- Java/src/ACODE.java
+++ Java/src/ACODE.java
@@ -18,23 +18,8 @@ public class ACODE {
     }
 
     private static int getTotalDecoding(String value) {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("0", 0);
+        return 0;
 
-        for (int i = 1; i < 10; i++) {
-            map.put(String.valueOf(i), 1);
-        }
-
-        String [] arr = value.split("");
-        return mergeDecoding(arr, map);
-    }
-
-    private static int mergeDecoding(String[] arr, Map<String, Integer> map) {
-        while (arr.length != 1) {
-            for (int i = 0; i < (arr.length / 2); i++) {
-                //merge arr[2*i] and arr[2*i + 1] and push to array of size arr.length / 2
-
-            }
-        }
+        //leaving this problem for now and moving to other problems
     }
 }
