cb55f01fb510a1c0b3f5afc91a6ae9f538c2bd20
Yogesh Singh <ysingh@gainsight.com>
2017-02-23 01:05:07 +0530

    Got the issue with prev code

--- Java/src/STAMPS.java
+++ Java/src/STAMPS.java
@@ -1,7 +1,41 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.*;
 
 /**
  * Created by YSingh on 22/02/17.
  */
-public class STAMPS {
+public class STAMPS  {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = Integer.parseInt(in.readLine());
+
+        for (int j = 0; j < n; j++) {
+            StringTokenizer tokenizer = new StringTokenizer(in.readLine());
+            int total = Integer.parseInt(tokenizer.nextToken());
+            int f = Integer.parseInt(tokenizer.nextToken());
+            int [] arr = new int[f];
+            tokenizer = new StringTokenizer(in.readLine());
+
+            for (int i = 0; i < f; i++) {
+                arr[i] = Integer.parseInt(tokenizer.nextToken());
+            }
+
+            Arrays.sort(arr);
+            out.printf("Scenario #%d:\n", j + 1);
+            for (int i = f-1; i >= 0; i--) {
+                total -= arr[i];
+                if (total <= 0) {
+                    out.println(f-i);
+                    break;
+                }
+            }
+            if (total > 0)
+                out.println("impossible");
+            out.println();
+        }
+        out.flush();
+    }
 }
